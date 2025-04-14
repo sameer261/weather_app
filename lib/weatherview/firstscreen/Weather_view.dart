ee57307e0eb02ef34a65f4215c64241b9f639998
Sameer Hussain <sameerhussain09473@gmail.com>
2025-04-14 05:50:52 +0500

    first commit

---
+++ lib/weatherview/firstscreen/Weather_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class firstscreen extends StatelessWidget {
+  const firstscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
