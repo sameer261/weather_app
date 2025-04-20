21ed87eeaf1cad72281db72af5bb8c208dc54c4f
Sameer Hussain <sameerhussain09473@gmail.com>
2025-04-20 06:01:39 +0500

    tabbar

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 // Color Variables
-Color peach = Color(0xFFFBC1AC);
-Color orange = Color(0xFFD6996B);
+Color peach = Color(0xffFEEBCF);
+Color orange = Color(0xffFDA654);
 
 // Radial Gradient Variable
 RadialGradient radialGradientBackground = RadialGradient(
-  center: Alignment.center,
-  radius: 1.0,
+  center: Alignment.topLeft,
+  radius: 2.0,
   colors: [peach, orange],
 );
 Color customDark = const Color(0xFF303344);
--- lib/weatherview/secondscreen/7days_weather_screen.dart
+++ lib/weatherview/secondscreen/7days_weather_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/utils/color.dart';
 
 class sevendaysWeatherScreen extends StatelessWidget {
   const sevendaysWeatherScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(gradient: radialGradientBackground),
+      ),
+    );
   }
 }
